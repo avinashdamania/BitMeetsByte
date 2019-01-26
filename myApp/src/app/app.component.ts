5ccd8dac6cfd0bc42fa2fd868806fd1b3f1b4e04
avinashdamania <avinashdamania@hotmail.com>
2019-01-26 17:28:38 -0600

    added find teams button and 4th tab

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -23,4 +23,5 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+  
 }
--- myApp/src/app/tab4/tab4.module.ts
+++ myApp/src/app/tab4/tab4.module.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab3Page } from './tab3.page';
+import { Tab4Page } from './tab4.page';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -13,8 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }])
+    RouterModule.forChild([{ path: '', component: Tab4Page }])
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab4Page]
 })
-export class Tab3PageModule {}
+export class Tab4PageModule {}
