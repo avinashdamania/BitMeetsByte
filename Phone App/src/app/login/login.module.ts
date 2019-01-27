4c773ef3d507184d1748b2cc73d8496c7e9de827
briannoogin <brian.qh.nguyen@gmail.com>
2019-01-27 06:50:12 -0600

    changed tab bar

---
+++ Phone App/src/app/login/login.module.ts
@@ -0,0 +1,25 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { LoginPage } from './login.page';
+
+import { TabsPageRoutingModule } from '../tabs/tabs.router.module';
+
+import { TabsPage } from '../tabs/tabs.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: LoginPage }])
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}
+
+
+
+
