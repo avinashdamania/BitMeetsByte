93a97b4384c1209f2bf41358157b8587e0c27f4f
astrobliss <reginaldfrank77@tamu.edu>
2019-01-26 20:37:28 -0600

    gen_database db method

--- User.py
+++ User.py
@@ -1,14 +1,14 @@
 from PIL import Image
 class User:
-    def __init__(self,first_name,last_name,age,bio,misc,count,img):
+    def __init__(self,first_name,last_name,age,bio,count,img,skill):
         self.first_name = first_name
         self.last_name = last_name 
         self.age = age
         self.bio = bio
-        self.misc = misc
         self.matches = []
         self.id = count
-        self.img= img
+        self.img = img
+        self.skill = skill
 
     def add_match(self,user):
         self.matches.append(user)
