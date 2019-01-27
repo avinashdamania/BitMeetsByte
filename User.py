123e85e392f129d05a7c608b903ec0d402ae41e1
astrobliss <reginaldfrank77@tamu.edu>
2019-01-26 20:50:20 -0600

    minor change

--- User.py
+++ User.py
@@ -1,6 +1,6 @@
 from PIL import Image
 class User:
-    def __init__(self,first_name,last_name,age,bio,count,img='null',skill):
+    def __init__(self,first_name,last_name,age,bio,count,img,skill):
         self.first_name = first_name
         self.last_name = last_name 
         self.age = age
@@ -8,7 +8,8 @@ class User:
         self.matches = []
         self.id = count
         self.img = img
-        self.skill = skill
+        skillDict = {0:"FrontEnd Dev",1:"BackEnd Dev",2:"Mobile Dev",3:"Web Dev"}
+        self.skill = lol[skillDict]
 
     def add_match(self,user):
         self.matches.append(user)
