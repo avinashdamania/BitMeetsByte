d0cb1c7c22b87fa16090d3549b6521d5646f3708
briannoogin <brian.qh.nguyen@gmail.com>
2019-01-26 20:14:03 -0600

    added main method for api

--- User.py
+++ User.py
@@ -1,6 +1,6 @@
 from PIL import Image
 class User:
-    def __init__(self,first_name,last_name,age,bio,misc,count,img):
+    def __init__(self,first_name,last_name,age,bio,misc,count,img='null'):
         self.first_name = first_name
         self.last_name = last_name 
         self.age = age
@@ -13,5 +13,3 @@ class User:
     def add_match(self,user):
         self.matches.append(user)
 
-    def bindaddy(self):
-        print("YASSSSS")z
